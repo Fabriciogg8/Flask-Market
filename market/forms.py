9351c2d9c3bae99f68f0761554017426ea5736ac
unknown <Administrador@NB-DACC-80.mgap.gub.uy>
2022-03-22 10:29:48 -0300

    Flash messages and advanced validations

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,26 @@
+from xml.dom import ValidationErr
+from market.models import User
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User name: ')
-    email_adress = StringField(label='Email adress: ')
-    password = PasswordField(label='Password: ')
-    confirm_password = PasswordField(label='Confirm password: ')
+    username = StringField(label='User name: ',validators=[Length(min=2, max=30), DataRequired()])
+    email_adress = StringField(label='Email adress: ', validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password: ', validators=[Length(min=6), DataRequired()])
+    confirm_password = PasswordField(label='Confirm password: ', validators=[EqualTo('password'), DataRequired()])
     submit = SubmitField(label='Create Account')
+
+
+    def validate_username(self, field):
+        user = User.query.filter_by(username=field.data).first()
+        if user:
+            raise ValidationError('Username already in use')
+
+
+    def validate_email_adress(self, field):
+        email = User.query.filter_by(email_adress=field.data).first()
+        if email:
+            raise ValidationError('Email already in use')
--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,9 @@
 from market import app
-from market.models import Item
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
-from flask import render_template
+from flask import redirect, render_template, url_for, flash
 
 
 @app.route('/')
@@ -17,7 +18,18 @@ def market_page():
     return render_template('market.html', items=items)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_create = User(username = form.username.data,
+                        email_adress = form.email_adress.data,
+                        password_hash = form.password.data)
+        db.session.add(user_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}: #If there are not errors from the validators
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user {err_msg}', category='danger')
     return render_template('register.html', form=form)
+
