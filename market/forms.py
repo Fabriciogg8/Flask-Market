1aa9e5d80bbbc0e1747435fbafdf7c58b1c90a6c
unknown <Administrador@NB-DACC-80.mgap.gub.uy>
2022-03-21 22:07:04 -0300

    Register form and template finished

--- market/forms.py
+++ market/forms.py
@@ -1,11 +1,10 @@
-import email
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='username')
-    email_adress = StringField(label='email')
-    password = PasswordField(label='password')
-    confirm_password = PasswordField(label='confirm_password')
-    submit = SubmitField(label='submit')
+    username = StringField(label='User name: ')
+    email_adress = StringField(label='Email adress: ')
+    password = PasswordField(label='Password: ')
+    confirm_password = PasswordField(label='Confirm password: ')
+    submit = SubmitField(label='Create Account')
